ef1b5cfe9f260b76d1a141a69d6de6188d72a2a2
copygirl <copygirl@mcft.net>
2021-01-19 21:11:11 +0100

    Load recipes from assets

--- src/BuildingOverhaulSystem.cs
+++ src/BuildingOverhaulSystem.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
+using Vintagestory.API.Util;
 using Vintagestory.Client.NoObf;
-using Vintagestory.GameContent;
-using HarmonyLib;
 
 [assembly: ModInfo("BuildingOverhaul",
 	Description = "",
@@ -19,9 +22,11 @@ namespace BuildingOverhaul
 	{
 		public const string MOD_ID = "buildingoverhaul";
 
-		public const string FAILURE_NO_HAMMER    = MOD_ID + "-nohammer";
 		public const string FAILURE_NO_RECIPE    = MOD_ID + "-norecipe";
 		public const string FAILURE_NO_MATERIALS = MOD_ID + "-nomaterials";
+
+		// FIXME: This won't work in multiplayer - need to send recipes to clients.
+		public static List<List<BuildingRecipe>> RecipesByTool { get; set; }
 
 		// Client
 		public static BuildingOverhaulSystem Instance { get; private set; }
@@ -45,6 +50,17 @@ namespace BuildingOverhaul
 			Harmony.PatchAll();
 		}
 
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			ServerAPI = api;
+
+			ServerChannel = api.Network.RegisterChannel(MOD_ID);
+			ServerChannel.RegisterMessageType<BuildingMessage>();
+			ServerChannel.SetMessageHandler<BuildingMessage>(OnBuildingMessage);
+
+			api.Event.SaveGameLoaded += OnSaveGameLoaded;
+		}
+
 		public override void Dispose()
 		{
 			if (ClientAPI != null) {
@@ -53,11 +69,37 @@ namespace BuildingOverhaul
 			}
 		}
 
-		public override void StartServerSide(ICoreServerAPI api)
+		public void OnSaveGameLoaded()
 		{
-			ServerChannel = api.Network.RegisterChannel(MOD_ID);
-			ServerChannel.RegisterMessageType<BuildingMessage>();
-			ServerChannel.SetMessageHandler<BuildingMessage>(OnBuildingMessage);
+			var assets  = ServerAPI.Assets.GetMany<JToken>(Mod.Logger, "recipes/" + MOD_ID);
+			var recipes = new List<BuildingRecipe>();
+
+			void LoadRecipe(AssetLocation location, JToken token)
+			{
+				var recipe = token.ToObject<BuildingRecipe>(location.Domain);
+				recipe.Location = location;
+				// Ensure that every ingredient's AllowedVariants is distinct and sorted.
+				foreach (var ingredient in new []{ recipe.Tool, recipe.Material }.Append(recipe.Ingredients))
+					ingredient.AllowedVariants = ingredient.AllowedVariants?.OrderBy(x => x)?.Distinct()?.ToArray();
+				recipes.Add(recipe);
+			}
+
+			foreach (var asset in assets)
+			switch (asset.Value) {
+				case JObject obj: LoadRecipe(asset.Key, obj); break;
+				case JArray arr: arr.Do(token => LoadRecipe(asset.Key, token)); break;
+				default: throw new Exception("Unexpected JToken type " + asset.Value.Type);
+			}
+
+			RecipesByTool = new();
+			foreach (var recipe in recipes) {
+				if (!recipe.Enabled) continue;
+				var groupedRecipes = RecipesByTool.Find(list => (recipe.Tool == list[0].Tool));
+				if (groupedRecipes == null) RecipesByTool.Add(groupedRecipes = new());
+				groupedRecipes.Add(recipe);
+			}
+
+			Mod.Logger.Event("{0} building recipes loaded", recipes.Count);
 		}
 
 		public bool OnInWorldInteract()
@@ -68,10 +110,8 @@ namespace BuildingOverhaul
 
 			var result = TryBuild(player, selection, true);
 			if (!result.IsSuccess) {
-				// Ignore missing hammer and missing recipe failures,
-				// this just means we aren't holding the right items.
-				if ((result.FailureCode == FAILURE_NO_HAMMER) ||
-				    (result.FailureCode == FAILURE_NO_RECIPE)) return false;
+				// Ignore missing recipe failures, this just means we aren't holding the right items.
+				if ((result.FailureCode == FAILURE_NO_RECIPE)) return false;
 				// But otherwise do show an error message.
 				ClientAPI.TriggerIngameError(this, result.FailureCode,
 					Lang.Get("placefailure-" + result.FailureCode, result.LangParams));
@@ -90,17 +130,44 @@ namespace BuildingOverhaul
 
 		private BuildResult TryBuild(IPlayer player, BlockSelection selection/*, shape? */, bool doOffset)
 		{
-			// TODO: Required tool shouldn't have to be a hammer.
 			var inventory = player.InventoryManager;
+
+			// Make sure the player is holding a valid tool in their offhand slot.
 			var offhandItem = inventory.GetOwnInventory(GlobalConstants.hotBarInvClassName)[10].Itemstack;
-			if (!(offhandItem.Item is ItemHammer))
-				return new(){ FailureCode = FAILURE_NO_HAMMER };
+			var recipes = RecipesByTool.Find(list => list[0].Tool.Matches(offhandItem));
+			if (recipes == null) return new(){ FailureCode = FAILURE_NO_RECIPE };
+
+			// Make sure there is a recipe that matches the material
+			// the player is holding in their active hotbar slot.
 			var hotbarItem = inventory.ActiveHotbarSlot.Itemstack;
-			if ((hotbarItem == null) || !hotbarItem.Collectible.Code.BeginsWith("game", "plank-"))
-				return new(){ FailureCode = FAILURE_NO_RECIPE };
+			if (hotbarItem == null) return new(){ FailureCode = FAILURE_NO_RECIPE };
+			var recipe = recipes.Find(r => r.Material.Matches(hotbarItem));
+			if (recipe == null) return new(){ FailureCode = FAILURE_NO_RECIPE };
+
+			var codes = new Dictionary<string, string>();
+			void AddNameToCodeMapping(Ingredient wildcard, ItemStack stack)
+			{
+				if (wildcard.Name == null) return;
+				var recipePath = wildcard.Code.Path;
+				var actualPath = stack.Collectible.Code.Path;
+				var wildcardIndex = recipePath.IndexOf('*');
+				var value = actualPath.Substring(wildcardIndex, actualPath.Length - recipePath.Length + 1);
+				codes.Add(wildcard.Name, value);
+			}
+			AddNameToCodeMapping(recipe.Tool, offhandItem);
+			AddNameToCodeMapping(recipe.Material, hotbarItem);
+
+			// TODO: Support wildcards in output?
+			var output = recipe.Output.Clone();
+			foreach (var code in codes)
+				output.Path = output.Path.Replace("{" + code.Key + "}", code.Value);
 
 			var world    = player.Entity.World;
-			var newBlock = world.GetBlock(new AssetLocation("game", $"planks-{hotbarItem.Collectible.LastCodePart()}"));
+			var newBlock = world.GetBlock(output);
+			if (newBlock == null) {
+				Mod.Logger.Warning("Could not find block '{0}' for recipe '{1}'", output, recipe.Location);
+				return new(){ FailureCode = FAILURE_NO_RECIPE };
+			}
 
 			if (doOffset) {
 				var clickedBlock = world.BlockAccessor.GetBlock(selection.Position);
@@ -150,6 +217,7 @@ namespace BuildingOverhaul
 				yield return new(OpCodes.Brfalse_S, falseLabel);
 				yield return new(OpCodes.Ret);
 
+				// Replace the following instruction so we can specify the label to jump to.
 				enumerator.MoveNext();
 				yield return new(enumerator.Current){ labels = new(){ falseLabel } };
 
---
+++ src/BuildingRecipe.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Vintagestory.API.Common;
+using Vintagestory.API.Util;
+
+namespace BuildingOverhaul
+{
+	public class BuildingRecipe
+	{
+		[JsonIgnore]
+		public AssetLocation Location { get; set; }
+
+		public string Shape { get; set; }
+		public Ingredient Tool { get; set; }          // Type, Name and Quantity not applicable.
+		public Ingredient Material { get; set; }      // Quantity not applicable.
+		public Ingredient[] Ingredients { get; set; } // Name not applicable.
+		public AssetLocation Output { get; set; }
+
+		public bool Enabled { get; } = true;
+		public int ToolDurabilityCost { get; } = 1;
+		// public AssetLocation ReplaceBlock { get; } = null;
+	}
+
+	public class Ingredient : IEquatable<Ingredient>
+	{
+		public EnumItemClass Type { get; set; } = EnumItemClass.Item;
+		public AssetLocation Code { get; set; }
+		public string[] AllowedVariants { get; set; } = null;
+
+		// Name and Quantity are not relevant for equality.
+		public string Name { get; set; } = null;
+		public int Quantity { get; set; } = 1;
+
+		internal bool Matches(ItemStack stack)
+			=> (Type == stack.Collectible.ItemClass) &&
+			   WildcardUtil.Match(Code, stack.Collectible.Code, AllowedVariants);
+
+		public bool Equals(Ingredient other)
+		{
+			return (other != null) && (Type == other.Type) && Code.Equals(other.Code) &&
+				((AllowedVariants == other.AllowedVariants) ||
+				 ((AllowedVariants != null) && (other.AllowedVariants != null) &&
+				  AllowedVariants.SequenceEqual(other.AllowedVariants)));
+		}
+
+		public override bool Equals(object obj)
+			=> Equals(obj as Ingredient);
+
+		public override int GetHashCode()
+		{
+			int hashCode = 1404618148;
+			hashCode = hashCode * -1521134295 + Type.GetHashCode();
+			hashCode = hashCode * -1521134295 + Code.GetHashCode();
+			if (AllowedVariants != null)
+			foreach (var variant in AllowedVariants)
+				hashCode = hashCode * -1521134295 + variant.GetHashCode();
+			return hashCode;
+		}
+
+		public static bool operator ==(Ingredient left, Ingredient right)
+			=> left?.Equals(right) ?? object.ReferenceEquals(left, right);
+		public static bool operator !=(Ingredient left, Ingredient right)
+			=> !(left == right);
+	}
+}
